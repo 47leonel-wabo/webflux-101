f9a8f762e4c812568847fb062d0acedc574328d8
leonel47 <waboleonel@gmail.com>
2023-11-30 20:38:15 +0100

    Light way functional endpoint with path predicate

--- src/main/java/com/wbt/webflux101/functional/config/RouteConfig.java
+++ src/main/java/com/wbt/webflux101/functional/config/RouteConfig.java
@@ -5,6 +5,7 @@ import com.wbt.webflux101.functional.RequestHandler;
 import com.wbt.webflux101.functional.error.ExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -29,6 +30,8 @@ public class RouteConfig {
         return RouterFunctions.route()
                 .GET("/square/{value}", this.requestHandler::squareRootHandler)
                 .GET("/square/{value}/valid", this.requestHandler::squareRootWithValidation2Handler)
+                .GET("/square/{value}/validation", RequestPredicates.path("*/1?"), this.requestHandler::squareRootWithValidation2Handler)
+                .GET("/square/{value}/validation", req -> ServerResponse.badRequest().bodyValue("Only values from 10 to 19 allowed!"))
                 .GET("/table/{value}", this.requestHandler::tableHandler) // streaming by default
                 .POST("/multiply", this.requestHandler::multiplicationHandler)
                 .onError(InvalidInputException.class, ExceptionHandler.handle())
