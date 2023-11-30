d28a0dc636378856876602532b3816a239c302f4
leonel47 <waboleonel@gmail.com>
2023-11-30 09:48:00 +0100

    Fixing missing bean annotation

--- src/main/java/com/wbt/webflux101/functional/config/RouteConfig.java
+++ src/main/java/com/wbt/webflux101/functional/config/RouteConfig.java
@@ -1,6 +1,7 @@
 package com.wbt.webflux101.functional.config;
 
 import com.wbt.webflux101.functional.RequestHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -15,10 +16,11 @@ public class RouteConfig {
         this.requestHandler = requestHandler;
     }
 
+    @Bean
     public RouterFunction<ServerResponse> serverResponse() {
         return RouterFunctions.route()
                 .GET("/functional/math/square/{value}", this.requestHandler::squareRootHandler)
-                .GET("/functional/math/table/{value}", this.requestHandler::tableHandler)
+                .GET("/functional/math/table/{value}", this.requestHandler::tableHandler) // streaming by default
                 .build();
     }
 
