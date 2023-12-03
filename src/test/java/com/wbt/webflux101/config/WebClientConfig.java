e202cc873a814d86d4dcbcfd5aaaedb75cdb4901
leonel47 <waboleonel@gmail.com>
2023-12-03 08:05:57 +0100

    Passing attribute to influence the configuration class

--- src/test/java/com/wbt/webflux101/config/WebClientConfig.java
+++ src/test/java/com/wbt/webflux101/config/WebClientConfig.java
@@ -3,7 +3,10 @@ package com.wbt.webflux101.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -11,12 +14,36 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080")
-                .filter((request, next) -> { // passing authorization
-                    final var clientRequest = ClientRequest.from(request)
-                            .headers(httpHeaders -> httpHeaders.setBearerAuth("secret-jwt-token-here!!"))
-                            .build();
-                    return next.exchange(clientRequest);
-                })
+                .filter(WebClientConfig::sessionToken)
+                .build();
+    }
+
+//    private static Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction next) {
+//        // passing authorization
+//        final var clientRequest = ClientRequest.from(request)
+//                .headers(httpHeaders -> httpHeaders.setBearerAuth(""))
+//                .build();
+//        return next.exchange(clientRequest);
+//    }
+
+    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction next) {
+        // ClientRequest is immutable
+        // looking for attribute set by the client requesting this method auth --> oauth or basic
+        final var clientRequest = request.attribute("auth")
+                .map(o -> o.equals("oauth") ? withOAuth(request) : withBasicAuth(request))
+                .orElse(request);
+        return next.exchange(clientRequest);
+    }
+
+    private ClientRequest withBasicAuth(final ClientRequest request) {
+        return ClientRequest.from(request)
+                .headers(httpHeaders -> httpHeaders.setBasicAuth("my-username", "my-password"))
+                .build();
+    }
+
+    private ClientRequest withOAuth(final ClientRequest request) {
+        return ClientRequest.from(request)
+                .headers(httpHeaders -> httpHeaders.setBearerAuth("lengthy-secret-jwt-token-here"))
                 .build();
     }
 }
