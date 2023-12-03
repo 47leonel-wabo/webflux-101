90b3d6b4164e465870d5871ab4292788ad4cd535
leonel47 <waboleonel@gmail.com>
2023-12-03 07:45:56 +0100

    Passing auth token

--- src/test/java/com/wbt/webflux101/config/WebClientConfig.java
+++ src/test/java/com/wbt/webflux101/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package com.wbt.webflux101.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -10,6 +11,12 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080")
+                .filter((request, next) -> { // passing authorization
+                    final var clientRequest = ClientRequest.from(request)
+                            .headers(httpHeaders -> httpHeaders.setBearerAuth("secret-jwt-token-here!!"))
+                            .build();
+                    return next.exchange(clientRequest);
+                })
                 .build();
     }
 }
